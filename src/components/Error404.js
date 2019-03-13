b3b572f2bf3d16eff4d88e41449a8e7d0baa85a2
Cristian Lucero <cristianjlucero32@gmail.com>
2019-03-13 15:08:52 -0700

    Added paypal sandbox

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 
 export default class Error404 extends Component {
   render() {
+    console.log(this.props);
     return (
-      <div>
-        <h1>page not found</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto text-center text-title">
+            <h1 className="display-3">404</h1>
+            <h2>Error</h2>
+            <h3>Page Not Found</h3>
+            <h4>
+              The URL pathname
+              <span className="text-danger">
+                '{this.props.location.pathname}'
+              </span>
+              was not found
+            </h4>
+          </div>
+        </div>
       </div>
     );
   }
